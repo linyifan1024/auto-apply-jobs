1601ddec12e8dee7cad62f809653a234c0063d0a
Yifan Lin <92171160+linyifan1024@users.noreply.github.com>
2023-04-02 04:34:23 -0500

    set up react frontend

--- auto_apply/api/views.py
+++ auto_apply/api/views.py
@@ -5,8 +5,16 @@ from .models import User, Platform, Job
 from .serializers import UserSerializer, PlatformSerializer, JobSerializer
 
 
+
 # Create your views here.
 class ProfileView(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    
+
+class PlatformView(generics.ListAPIView):
+    queryset = Platform.objects.all()
+    serializer_class = PlatformSerializer
+
+class JobView(generics.ListAPIView):
+    queryset = Job.objects.all()
+    serializer_class = JobSerializer
