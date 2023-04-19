d729bae2d2190b169c71bbc8018e7b98f86f6039
Yifan Lin <92171160+linyifan1024@users.noreply.github.com>
2023-04-18 21:25:52 -0500

    Update App.js

--- auto_apply/frontend/src/components/App.js
+++ auto_apply/frontend/src/components/App.js
@@ -1,35 +1,35 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React, { Component } from "react";
+import { render } from "react-dom";
 import NavBar from "./NavBar";
 import HomePage from "./HomePage";
 import RegisterPage from "./RegisterPage";
 import Glassdoor from "./Glassdoor";
 import Indeed from "./Indeed";
 import Linkedin from "./Linkedin";
-import {
-  BrowserRouter,
-  Router,
-  Routes,
-  Switch,
-  Route,
-  Link,
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 export default class App extends Component {
   constructor(props) {
     super(props);
   }
 
-ReactDOM.createRoot(document.getElementById("app")).render(
-  <React.StrictMode>
-    <Router>
-      <NavBar />
-      <Routes>
-        <Route exact path="/" element={<HomePage />} />
-        <Route path="/linkedin" element={<Linkedin />} />
-        <Route path="/glassdoor" element={<Glassdoor />} />
-        <Route path="/indeed" element={<Indeed />} />
-        <Route path="/register" element={<RegisterPage />} />
-      </Routes>
-    </Router>
-  </React.StrictMode>
-);
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <NavBar />
+          <Routes>
+            <Route exact path="/" element={<HomePage />} />
+            <Route path="/linkedin" element={<Linkedin />} />
+            <Route path="/glassdoor" element={<Glassdoor />} />
+            <Route path="/indeed" element={<Indeed />} />
+            <Route path="/register" element={<RegisterPage />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+const appDiv = document.getElementById("app");
+render(<App />, appDiv);
